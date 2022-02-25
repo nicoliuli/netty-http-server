b8511b187417a86f8f05f5d675d3c8a8889b8d30
li.liu <li.liu@ilivedata.com>
2022-02-25 14:22:06 +0800

    init project

---
+++ src/main/java/com/nico/NettyHttpServerApplication.java
@@ -0,0 +1,13 @@
+package com.nico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyHttpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyHttpServerApplication.class, args);
+	}
+
+}
